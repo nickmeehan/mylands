04948fbd591089d678e0ab795896bb86267ef73f
Nick Meehan <nnmeehan@gmail.com>
2014-07-27 06:57:33 -0700

    Changing location controller and map controller to application controller.

--- app/assets/javascripts/mapView.js
+++ app/assets/javascripts/mapView.js
@@ -1,3 +1,28 @@
-function MapView() {
+function MapView(mapOverlay) {
+  this.imageUrl = mapOverlay['imageUrl']
+  this.imageBounds = mapOverlay['imageBounds']
+}
 
+MapView.prototype = {
+  overlayMapImage: function(map) {
+    L.imageOverlay(this.imageUrl, this.imageBounds).addTo(map)
+  },
+  appendMarker: function(map, checkin) {
+    L.mapbox.featureLayer({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [
+              checkin.longitude,
+              checkin.latitude
+            ]
+        },
+        properties: {
+            title: 'Peregrine Espresso',
+            description: '1718 14th St NW, Washington, DC',
+            'marker-size': 'large',
+            'marker-color': '#000'
+        }
+    }).addTo(map);
+  }
 }
