4f0f54d7085472dd612b17c4806ec7210b894160
Nick Meehan <nnmeehan@gmail.com>
2014-07-26 18:57:22 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mylands::Application.config.secret_key_base = 'dc9639eadbdae36fb81f05c48f60ab60d61d011b46bb094128853bc79f7907c3762b65cde2b5d85fbc1e74074f2ed9bc0144991ca20c3d611ef3d2ffd4eebfa9'
