da8c6e9f2cea21c8cea93ce3b084a248487632ca
Nick Meehan <nnmeehan@gmail.com>
2014-07-27 12:17:50 -0700

    Adding whole playlist stuff. Too much to say. I hope this doesn't break EVERYTHING

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,9 +1,14 @@
 $(document).ready( function () {
-  new ApplicationController(new LocationView(locationViewSelectors), new MapView(mapOverlay), mapOptions).init();
+  new ApplicationController(new LocationView(locationViewSelectors), new MapView(mapOverlay), mapOptions, new CheckinView(checkinViewSelectors)).init();
 })
 
 var locationViewSelectors = {
   findmeButton: '#new_checkin'
+}
+
+var checkinViewSelectors = {
+  checkinsWithArtist: '.checkin-artist',
+  exitButton: '.exit'
 }
 
 var mapOptions = {
@@ -18,4 +23,8 @@ var mapOptions = {
 var mapOverlay = {
   imageUrl: 'http://i.imgur.com/My9ribO.png',
   imageBounds: [[37.766342,-122.4959086], [37.7701691,-122.483093]]
+}
+
+String.prototype.capitalize = function() {
+    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
 }
