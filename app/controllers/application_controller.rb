dd419145ae7f0ac9b75414a9942586a80a055209
Nick Meehan <nnmeehan@gmail.com>
2014-07-27 12:32:16 -0700

    Removing Sterling's binding pry from ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    binding.pry
     if session[:user_id]
       @current_user = User.find(session[:user_id])
     end
